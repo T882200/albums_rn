8825b96bc93a60cdfa30a71839b64eb67f14b5d2
t8822_000 <t882200@gmail.com>
2019-01-12 23:38:11 +0200

    Adding images to the album cards

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,9 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 // create a component
 class AlbumList extends Component {
@@ -14,11 +16,18 @@ class AlbumList extends Component {
     axios.get('https://http://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
     return (
-      <View>
-        <Text>Album List!</Text>
-      </View>
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
